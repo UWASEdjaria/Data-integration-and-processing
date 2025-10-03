d5b7d6f2c22477ebd26a917c305c5914f1349833
Djaria UWASE <uwasedjaria41@gmail.com>
2025-10-03 09:48:53 +0200

    fetch api

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Question 1: Merging Author and Comment Data
+
+async function getArticleId(articleId){
+ const response=await fetch(`https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user/${articleId}`);
+try {
+
+    if(!response.ok){
+        throw new Error("HTTP error:",response.status);
+      
+    }  
+    const data=await response.json();
+    return data;
+} catch (error) {
+    console.log("HTTP error:",error);
+}
+};
+getArticleId();
+
+async function articleId(){}
+
